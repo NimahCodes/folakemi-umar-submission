8b17606e0044afb6a78da0f79c8293752e30b014
FOLAKEMI UMAR <folake.umar@gmail.com>
2022-03-17 04:27:36 +0100

    implemented second logic

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BookDetailsView
+from .views import BookDetailsView, AuthorBookView, AuthorBookCountView
 # from .views import AuthorView
 
 urlpatterns = [
     path('api/v1/book-details/', BookDetailsView.as_view(), name='book-details'),
+    path('api/v1/author-books/', AuthorBookView.as_view(), name='author-books'),
+    path('api/v1/author-books-count/',
+         AuthorBookCountView.as_view(), name='author-books-count'),
     # path('api/v1/author/', AuthorView.as_view(), name='author'),
-]  
+]
--- book/views.py
+++ book/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from .models import Author, Book
 from .serializers import BookSerializers, AuthorBookSerializers
-import operator
+from itertools import groupby
 
 
 def get_title_and_author(queryset):
@@ -41,5 +41,65 @@ class BookDetailsView(generics.CreateAPIView):
             )
         except Exception as e:
             return Response(
-                dict(message="falied", data=None, error=f'{e}'), status=status.HTTP_400_BAD_REQUEST
+                dict(message="failed", data=None, error=f'{e}'), status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class AuthorBookView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = AuthorBookSerializers
+
+    def get(self, request, *args, **kwargs):
+        '''
+        4.2 Write another function that will print the author’s 
+        name and all the books he wrote. For all the authors 
+        we have in the database. Like this:
+        Leo Tolstoy: “War and Peace”, “Anna Karenina”, “Resurrection”
+        Alexandre Dumas: “The Three Musketeers”, “The Count of Monte Cristo”
+        '''
+        try:
+            result = list(map(
+                lambda d: {Author.objects.get(name=d.get("name")).name:
+                           Book.objects.filter(
+                               author=Author.objects.get(name=d.get("name")))
+                           .values_list('title', flat=True).distinct()
+                           }, Author.objects.values('name')
+            ))
+            return Response(
+                dict(message="success", data=result, error=None), status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                dict(message="failed", data=None, error=f'{e}'), status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class AuthorBookCountView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = AuthorBookSerializers
+
+    def get(self, request, *args, **kwargs):
+        '''
+        4.3 Implement the third function, it should print 
+        the author’s name and the number of books he wrote. 
+        Order by the number of books written, descending. Like this:
+        Leo Tolstoy: 3
+        Alexandre Dumas: 2
+        '''
+        try:
+            result = list(map(
+                lambda d: {Author.objects.get(name=d.get("name")).name:
+                           len(Book.objects.filter(
+                               author=Author.objects.get(name=d.get("name")))
+                           .values_list('title', flat=True).distinct())
+                           }, Author.objects.values('name')
+            ))
+            return Response(
+                dict(message="success", data=result, error=None), status=status.HTTP_200_OK
+            )
+        except Exception as e:
+            return Response(
+                dict(message="failed", data=None, error=f'{e}'), status=status.HTTP_400_BAD_REQUEST
             )
